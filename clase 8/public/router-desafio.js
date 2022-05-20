aff76a66689f88aae88c16647f93dcb40fb61c4b
AaronJH21 <93291172+AaronJH21@users.noreply.github.com>
2022-05-19 23:44:57 -0300

    Add files via upload

---
+++ clase 8/public/router-desafio.js
@@ -0,0 +1,17 @@
+const express= require('express')
+const mascotasRouter =require ('./mascotasRouter')
+const personasRouter = require ('./personasRouter')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/mascotas',mascotasRouter)
+app.use('/personas',personasRouter)
+
+const PORT= 8080
+const server = app.listen(PORT,()=>{
+    console.log(`Servidor HTTP escuchando en el puerto${PORT}`)
+})
+server.on('error', error=> console.log(`Error en servidor:${error}`))
